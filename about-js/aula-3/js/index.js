e458555401a36fc37ca855db802fde7cbb89470e
ribeirosofia <sofia.ribeiro@creditas.com>
2023-04-20 19:24:21 -0300

    feat: exercise aula-3

---
+++ about-js/aula-3/js/index.js
@@ -0,0 +1,10 @@
+const data = new Date("2023-05-05 05:05:05");
+console.log("Dia", data.getDate());
+console.log("Mês", data.getMonth() + 1);
+console.log("Ano", data.getFullYear());
+console.log("Hora", data.getHours());
+console.log("Min", data.getMinutes());
+console.log("Seg", data.getSeconds());
+console.log("ms", data.getMilliseconds());
+console.log("Dia semana", data.getDay());
+console.log(data.toString());
