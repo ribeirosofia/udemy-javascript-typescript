8ef5357835d41be9cc19d86b33d3eef68c14259b
ribeirosofia <sofia.ribeiro@creditas.com>
2023-05-03 16:10:36 -0300

    feat:  aula-7 content (for)

---
+++ about-js/aula-7/index.js
@@ -0,0 +1,13 @@
+const fruits = [
+    "apple",
+    "strawberry",
+    "pineapple",
+    "grapefruit",
+    "orange",
+    "banana",
+  ];
+  
+  for (let i = 0; i < fruits.length; i++) {
+    console.log(`Índice ${i}`, fruits[i]);
+  }
+  
