1cbaf21f2f7565d3a0540eae76b3513124591518
ribeirosofia <sofia.ribeiro@creditas.com>
2023-05-07 22:48:54 -0300

    feat: create logica-2 exercise

---
+++ about-js/logica-2/index.js
@@ -0,0 +1,9 @@
+//Escreva uma função chamada landscapeImg que
+// recebe dois argumentos, largura e altura
+// de uma imagem (number).
+// Retorne true se a imagem estiver no modo
+// paisagem.
+
+const landscapeImg = (width, height) => width > height;
+
+console.log(landscapeImg(1080, 1920));
