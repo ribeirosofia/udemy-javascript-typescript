e49b7a5e8e943b2616e394f9205d59ee0fcc3148
ribeirosofia <sofia.ribeiro@creditas.com>
2023-05-09 19:11:27 -0300

    feat: create aula-13 file (finally)

---
+++ about-js/aula-13/index.js
@@ -0,0 +1,10 @@
+try {
+  console.log(neb);
+  console.log("Open file");
+  console.log("Change file and throw error");
+  console.log("Closed file");
+} catch (e) {
+  console.log("Fixing error");
+} finally {
+  console.log("The end");
+}
