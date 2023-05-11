2a16b3795ad024e3586f626e47ec1654cb1e5988
ribeirosofia <sofia.ribeiro@creditas.com>
2023-05-11 19:17:05 -0300

    refactor:  insert clock function

--- about-js/timer-exercise/assets/js/main.js
+++ about-js/timer-exercise/assets/js/main.js
@@ -1,39 +1,43 @@
-const displaySeconds = (seconds) => {
-  const date = new Date(seconds * 1000);
-  return date.toLocaleTimeString("pt-BR", {
-    hour12: false,
-    timeZone: "GMT",
+const clock = () =>{
+  const displaySeconds = (seconds) => {
+    const date = new Date(seconds * 1000);
+    return date.toLocaleTimeString("pt-BR", {
+      hour12: false,
+      timeZone: "GMT",
+    });
+  };
+  
+  const clock = document.querySelector(".clock");
+  const start = document.querySelector(".start");
+  const pause = document.querySelector(".pause");
+  const stopClock = document.querySelector(".stop");
+  let seconds = 0;
+  let timer;
+  
+  const startTime = () => {
+    timer = setInterval(() => {
+      seconds++;
+      clock.innerHTML = displaySeconds(seconds);
+    }, 1000);
+  };
+  
+  start.addEventListener("click", (event) => {
+    clock.classList.remove("pause-clock");
+    clearInterval(timer);
+    startTime();
   });
-};
+  
+  pause.addEventListener("click", (event) => {
+    clearInterval(timer);
+    clock.classList.add("pause-clock");
+  });
+  
+  stopClock.addEventListener("click", (event) => {
+    clearInterval(timer);
+    clock.innerHTML = "00:00:00";
+    clock.classList.remove("pause-clock");
+    seconds = 0;
+  });
+  
+}
 
-const clock = document.querySelector(".clock");
-const start = document.querySelector(".start");
-const pause = document.querySelector(".pause");
-const stopClock = document.querySelector(".stop");
-let seconds = 0;
-let timer;
-
-const startTime = () => {
-  timer = setInterval(() => {
-    seconds++;
-    clock.innerHTML = displaySeconds(seconds);
-  }, 1000);
-};
-
-start.addEventListener("click", (event) => {
-  clock.classList.remove("pause-clock");
-  clearInterval(timer);
-  startTime();
-});
-
-pause.addEventListener("click", (event) => {
-  clearInterval(timer);
-  clock.classList.add("pause-clock");
-});
-
-stopClock.addEventListener("click", (event) => {
-  clearInterval(timer);
-  clock.innerHTML = "00:00:00";
-  clock.classList.remove("pause-clock");
-  seconds = 0;
-});
