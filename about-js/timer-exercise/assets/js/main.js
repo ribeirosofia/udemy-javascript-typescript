d107a7745889b147ef970dd735a4b0c54da197a2
ribeirosofia <sofia.ribeiro@creditas.com>
2023-05-15 17:17:00 -0300

    refactor: adjust main file

--- about-js/timer-exercise/assets/js/main.js
+++ about-js/timer-exercise/assets/js/main.js
@@ -1,4 +1,4 @@
-const clock = () =>{
+const clock = () => {
   const displaySeconds = (seconds) => {
     const date = new Date(seconds * 1000);
     return date.toLocaleTimeString("pt-BR", {
@@ -6,38 +6,39 @@ const clock = () =>{
       timeZone: "GMT",
     });
   };
-  
+
   const clock = document.querySelector(".clock");
   const start = document.querySelector(".start");
   const pause = document.querySelector(".pause");
   const stopClock = document.querySelector(".stop");
   let seconds = 0;
   let timer;
-  
+
   const startTime = () => {
     timer = setInterval(() => {
       seconds++;
       clock.innerHTML = displaySeconds(seconds);
     }, 1000);
   };
-  
+  // document.addEventListener('click', (e)=>{
+  // const el = e.target;
+  // if (el.classList.contains('stop')){}
+  // if (el.classList.contains('start')){}
+  // if (el.classList.contains('pause')){}
+  //})
   start.addEventListener("click", (event) => {
     clock.classList.remove("pause-clock");
     clearInterval(timer);
     startTime();
   });
-  
   pause.addEventListener("click", (event) => {
     clearInterval(timer);
     clock.classList.add("pause-clock");
   });
-  
   stopClock.addEventListener("click", (event) => {
     clearInterval(timer);
     clock.innerHTML = "00:00:00";
     clock.classList.remove("pause-clock");
     seconds = 0;
   });
-  
-}
-
+};
