80e38f039361e44da4e248fe539211445948c3e3
ribeirosofia <sofia.ribeiro@creditas.com>
2023-06-05 22:25:15 -0300

    feat: create aula-25 file (method splice)

---
+++ about-js/aula-25/index.js
@@ -0,0 +1,17 @@
+const names = ["Maria", "João", "Eduardo", "Gabriel", "Julia"];
+
+// names.splice(index atual, delete, add(element)1,add(element)2);
+
+// pop:
+// const removeNames = names.splice(-1, 1);
+
+// shift:
+// const removeNames = names.splice(0, 1);
+
+// push:
+// const addNames = names.splice(names.length, 0, "Luiz");
+
+// unshift:
+// const addNames = names.splice(0, 0, "Luiz");
+
+// console.log(names, addNames);
