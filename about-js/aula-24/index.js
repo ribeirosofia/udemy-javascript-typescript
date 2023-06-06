b0dfd26146f9c068c4131c2ca064b3e760d08c6f
ribeirosofia <sofia.ribeiro@creditas.com>
2023-06-05 21:58:51 -0300

    feat: creation of aula-24 file (array methods)

---
+++ about-js/aula-24/index.js
@@ -0,0 +1,12 @@
+// const names = ["Phillip", "Jane", "Marie", "Bella", "Karen"];
+// const newArray = names.slice(1, -1);
+
+// const removedName = names.pop();
+// const removedName = names.shift();
+// const addName = names.push("Johnny");
+// const addName = names.unshift("Johhny");
+// console.log(newArray);
+
+const names = ["Chaya, Pinkhasovna, Lispector"];
+const name = names.join(" ");
+console.log(name);
