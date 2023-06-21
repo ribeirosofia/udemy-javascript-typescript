e02e223f94d82c3acf52ef5b27fc6a27d7084ba7
ribeirosofia <sofia.ribeiro@creditas.com>
2023-06-21 19:36:59 -0300

    feat: aula-38 (heritance)

---
+++ about-js/aula-38 (heritance)/index.js
@@ -0,0 +1,57 @@
+function Product(name, price) {
+  this.name = name;
+  this.price = price;
+}
+
+Product.prototype.increase = (amount) => {
+  this.price += amount;
+};
+Product.prototype.increase = (amount) => {
+  this.price -= amount;
+};
+
+function Shirt(name, price, color) {
+  Product.call(this, name, price);
+  this.color = color;
+}
+
+Shirt.prototype = Object.create(Product.prototype);
+Shirt.prototype.constructor = Shirt;
+
+Shirt.prototype.increase = (percentage) => {
+  this.price += percentage / 100;
+};
+
+function Mug(name, price, material, storage) {
+  Product.call(this, name, price);
+  this.material = material;
+  this.storage = storage;
+
+  Object.defineProperty(this, "storage", {
+    enumerable: true,
+    configurable: false,
+    get: function () {
+      return storage;
+    },
+    set: function (value) {
+      if (typeof value !== "number") return;
+      storage = value;
+    },
+  });
+}
+
+Mug.prototype = Object.create(Product.prototype);
+Mug.prototype.constructor = Mug;
+
+Mug.prototype.discount = (percentage) => {
+  this.price -= percentage / 100;
+};
+
+const product = new Product("Generic", 111);
+const shirt = new Shirt("Sleeves", 7.5, "gray");
+const mug = new Mug("Frog mug", 12, "clay", 5);
+shirt.increase(10);
+mug.discount(2);
+console.log(product);
+console.log(shirt);
+console.log(mug);
